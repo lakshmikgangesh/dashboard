a9c6a29751a46a3f859d16291c1097eaa1173f8a
lakshmikgangesh <lakshmikgangesh@gmail.com>
2020-10-23 04:59:54 +0530

    initial commit

---
+++ assets/scss/process.php
@@ -0,0 +1,16 @@
+﻿<?php
+include_once 'database.php';
+if(isset($_POST['btn-save']))
+{
+// variables for input data
+$userId = $_POST['userId'];
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$city_name = $_POST['city_name'];
+$email = $_POST['email'];
+// sql query for inserting data into database
+
+mysqli_query($conn,"insert into myusers(userId,first_name,last_name,city_name,email) values ('$userId', '$first_name','$last_name','$city_name','$email')") or die('Error: ' . mysqli_error($conn));
+echo "<p align=center>Data Added Successfully.</p>";
+}
+?>
---
+++ public/js/attendance.js
@@ -0,0 +1,16 @@
+function CountFun1()
+  {
+    document.getElementById("c1").innerHTML = Math.round((parseInt(document.getElementById("t1").value)/(parseInt(document.getElementById("t1").value)+parseInt(document.getElementById("k1").value)))*100);
+  }
+  function CountFun2()
+    {
+      document.getElementById("c2").innerHTML = Math.round((parseInt(document.getElementById("t2").value)/(parseInt(document.getElementById("t2").value)+parseInt(document.getElementById("k2").value)))*100);
+    }
+    function CountFun3()
+      {
+        document.getElementById("c3").innerHTML = Math.round((parseInt(document.getElementById("t3").value)/(parseInt(document.getElementById("t3").value)+parseInt(document.getElementById("k3").value)))*100);
+      }
+      function CountFun4()
+        {
+          document.getElementById("c4").innerHTML = Math.round((parseInt(document.getElementById("t4").value)/(parseInt(document.getElementById("t4").value)+parseInt(document.getElementById("k4").value)))*100);
+        }
